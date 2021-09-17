7c3b6110c23d97f1a52e8d1a3611bb4c15b38069
Anger Romain <anger.romain@gmail.com>
2021-09-17 11:13:47 +0200

    ajout de commentaires pour la lisibilité du code

--- js/chi_fu_mi.js
+++ js/chi_fu_mi.js
@@ -17,16 +17,22 @@ alert("Vous avez choisi \"" + userChoice + "\".\nC\'est maintenant au tour de l\
 var computerChoice;
 var resultChoice = Math.random();
 
+// condition du choix de la pierre sur les premier 33% du random
 if (resultChoice <= 0.33){
     computerChoice = "pierre";
+    // message pour annoncé le choix de la pierre par l'ordinateur
     alert("L\'ordinateur a choisi \"" + computerChoice + "\"");
 }
+// condition du choix de la feuille entre les 33% et 66% du random
 else if(resultChoice > 0.33 && resultChoice <= 0.66){
     computerChoice = "feuille";
+    // message pour annoncé le choix de la feuille par l'ordinateur
     alert("L\'ordinateur a choisi \"" + computerChoice + "\"");
 }
+// condition choix des ciseaux pour les derniers 33% du random
 else {
     computerChoice = "ciseaux";
+    // message pour annoncé le choix du ciseaux par l'ordinateur
     alert("L\'ordinateur a choisi \"" + computerChoice + "\"");
 }
 
@@ -36,6 +42,7 @@ var computerGameWin = 0;
 
 // comparaison de match nul
 if (computerChoice === userChoice.toLocaleLowerCase()){
+    // message en cas de match nul
     alert("Il y a match nul");
 }
 // comparaison des possibilités de gain de l'ordinateur
@@ -43,10 +50,12 @@ else if ((computerChoice === "pierre" && userChoice.toLocaleLowerCase() === "cis
         || (computerChoice === "feuille" && userChoice.toLocaleLowerCase() === "pierre") 
         || (computerChoice === "ciseaux" && userChoice.toLocaleLowerCase() === "feuille")){
     computerGameWin += 1;
+    // message pour annoncé la défaite
     alert("Vous avez perdu");
 }
 //  sinon gain du joueur
 else{
     userGameWin += 1;
+    // message pour annoncé le gain
     alert("Vous avez gagné 1 manche");
 }
