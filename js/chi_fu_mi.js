b1ffe9bca27f989f3d05ad7a3fc265e7b56b9bb9
Anger Romain <anger.romain@gmail.com>
2021-09-17 13:03:45 +0200

    changement complet de la présentation et ajout d'une deuxieme fonction

--- js/chi_fu_mi.js
+++ js/chi_fu_mi.js
@@ -1,22 +1,15 @@
-// affichage du message de bienvenue
-alert("Bienvenue sur le jeu du \"Pierre - Feuille - Ciseaux\"");
+//---------------------------------DECLARATION DES VARIABLES---------------------------------------
 
-// capture du prénom du candidat
-var userName = prompt("Merci d'entré votre Prénom :");
-
-// message de bienvenue incluant son prénom
-alert("Bienvenue " + userName + ", nous allons commencé la partie");
-
-// capture du choix du candidat
-var userChoice = prompt("Veuillez choisir entre \"pierre\" \"feuille\" \"ciseaux\"");
-
-// message de confirmation du choix du candidat
-alert("Vous avez choisi \"" + userChoice + "\".\nC\'est maintenant au tour de l\'ordinateur de jouer.");
-
-// conception de la fonction du choix de l'ordinateur
+// variable pour la fonction du choix de l'ordinateur
 var randChoice = Math.random();
-var pcChoice;
+var pcChoice = computerChoice(randChoice);
+// variable pour la fonction de comparaison des choix ordi/joueur
+var userGameWin = 0;
+var computerGameWin = 0;
 
+//---------------------------------CONCEPTION DES FONCTIONS---------------------------------------
+
+// fonction du choix de l'ordinateur
 function computerChoice(randChoice){
     if (randChoice <= 0.33){
         return pcChoice = "pierre";        
@@ -29,26 +22,48 @@ function computerChoice(randChoice){
     }
 }
 
+// fonction de comparaison des choix ordi/joueur
+function compareChoice(pcChoice, userChoice){
+    // comparaison de match nul
+    if (pcChoice === userChoice.toLocaleLowerCase()){
+        alert("Il y a match nul");
+    }
+    // comparaison des possibilités de gain de l'ordinateur
+    else if ((computerChoice(randChoice) === "pierre" && userChoice.toLocaleLowerCase() === "ciseaux") 
+            || (computerChoice(randChoice) === "feuille" && userChoice.toLocaleLowerCase() === "pierre") 
+            || (computerChoice(randChoice) === "ciseaux" && userChoice.toLocaleLowerCase() === "feuille")){        
+        alert("Vous avez perdu");
+        return computerGameWin += 1;
+    }
+    //  sinon gain du joueur
+    else{        
+        alert("Vous avez gagné 1 manche");
+        return userGameWin += 1;
+    }
+ }
+
+//---------------------------------DEROULEMENT DU JEU---------------------------------------
+
+// affichage du message de bienvenue
+alert("Bienvenue sur le jeu du \"Pierre - Feuille - Ciseaux\"");
+
+// capture du prénom du candidat
+var userName = prompt("Merci d'entrer votre Prénom :");
+
+// message de bienvenue incluant son prénom
+alert("Bienvenue " + userName + ", nous allons commencé la partie");
+
+//---------------------------------DEROULEMENT DU JEU EN BOUCLE---------------------------------
+//---------------------------------POUR UNE PARTIE EN 3 MANCHES---------------------------------
+
+// capture du choix du candidat
+var userChoice = prompt("Veuillez choisir entre \"pierre\" \"feuille\" \"ciseaux\"");
+
+// message de confirmation du choix du candidat
+alert("Vous avez choisi \"" + userChoice + "\".\nC\'est maintenant au tour de l\'ordinateur de jouer.");
+
 // affichage de ce que l'ordinateur a choisi
-alert("L\'ordinateur a choisi \"" + computerChoice(randChoice) + "\"")
+alert("L\'ordinateur a choisi \"" + computerChoice(randChoice) + "\"");
 
-// comparaison des choix ordi/joueur
-var userGameWin = 0;
-var computerGameWin = 0;
-
-// comparaison de match nul
-if (computerChoice === userChoice.toLocaleLowerCase()){
-    alert("Il y a match nul");
-}
-// comparaison des possibilités de gain de l'ordinateur
-else if ((computerChoice === "pierre" && userChoice.toLocaleLowerCase() === "ciseaux") 
-        || (computerChoice === "feuille" && userChoice.toLocaleLowerCase() === "pierre") 
-        || (computerChoice === "ciseaux" && userChoice.toLocaleLowerCase() === "feuille")){
-    computerGameWin += 1;
-    alert("Vous avez perdu");
-}
-//  sinon gain du joueur
-else{
-    userGameWin += 1;
-    alert("Vous avez gagné 1 manche");
-}
+// fonction de comparaison des choix ordi/joueur
+compareChoice(pcChoice, userChoice);
