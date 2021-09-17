822f0ffd0f74115ebb870c2db8959b06a5455193
Anger Romain <anger.romain@gmail.com>
2021-09-17 08:53:03 +0200

    les 4 premieres etapes du jeu sont créé

---
+++ js/chi_fu_mi.js
@@ -0,0 +1,12 @@
+// affichage du message de bienvenue
+alert("Bienvenue sur le jeu du \"Pierre - Feuille - Ciseaux\"");
+
+// capture du prénom du candidat
+var userName = prompt("Merci d'entré votre Prénom :");
+
+// message de bienvenue incluant son prénom
+alert("Bienvenue " + userName + ", nous allons commencé la partie");
+
+// capture du choix du candidat
+var userChoice = prompt("Veuillez choisir entre \"pierre\" \"feuille\" \"ciseaux\"");
+
