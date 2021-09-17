24618951afe625a5d3880272996e28a70d944bf2
Anger Romain <anger.romain@gmail.com>
2021-09-17 09:32:14 +0200

    ajout de la fonction du choix de l'ordinateur

--- js/chi_fu_mi.js
+++ js/chi_fu_mi.js
@@ -10,3 +10,18 @@ alert("Bienvenue " + userName + ", nous allons commencé la partie");
 // capture du choix du candidat
 var userChoice = prompt("Veuillez choisir entre \"pierre\" \"feuille\" \"ciseaux\"");
 
+// message de confirmation du choix du candidat
+alert("Vous avez choisi \"" + userChoice + "\".\nC\'est maintenant au tour de l\'ordinateur de jouer.");
+
+// conception du choix de l'ordinateur
+var computerChoice = Math.random();
+
+if (computerChoice <= 0.33){
+    alert("L\'ordinateur a choisi \"Pierre\"")
+}
+else if(computerChoice > 0.33 && computerChoice <= 0.66){
+    alert("L\'ordinateur a choisi \"Feuille\"")
+}
+else {
+    alert("L\'ordinateur a choisi \"Ciseaux\"")
+}
